ce32c9984c9770a67393cca77e0c5594be55fcca
Mitrius <S.Strehlau@europe.com>
2017-06-03 15:41:45 +0200

    Dodany nowy request

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
 	max_game_id = int(input("wpisz maksymalne id meczu: "))
 	pro_matches = requests.request_pro_matches(max_game_id)
 	pro_match_game_data = map(lambda x: requests.get_match_info(x['match_id']), pro_matches)
-	pro_players = list(chain.from_iterable(map(lambda x: (x['players']), pro_match_game_data)))
-	player_benchmark_list = list()
-	for entry in pro_players:
-		player_benchmark_list.append((entry['account_id'], entry['benchmarks']))
-	print("pobrano {0} statystyk".format((len(player_benchmark_list))))
+	pro_players = chain.from_iterable(map(lambda x: (x['players']), pro_match_game_data))
+	player_recent_matches = list(map(lambda x: requests.get_player_recent_history(x['account_id']), pro_players))
+
+	print("pobrano {0} statystyk".format((len(player_recent_matches))))
--- requests.py
+++ requests.py
@@ -1,10 +1,13 @@
 import json
+import urllib
 import urllib.request
 
 
 def request_pro_matches(_max_id: int) -> dict:
 	url = "https://api.opendota.com/api/proMatches?less_than_match_id={0}".format(_max_id)
-	pro_players_games = urllib.request.urlopen(url).read()  # gry zagrane o mniejszym id od wpisanego
+	req = urllib.request.Request(url)
+	req.add_header('User-Agent', 'super happy dosBot')
+	pro_players_games = urllib.request.urlopen(req).read()  # gry zagrane o mniejszym id od wpisanego
 	data = json.loads(pro_players_games.decode('utf8'))
 
 	print("Wczytano dane o {0} meczach".format(len(data)))
@@ -13,5 +16,15 @@ def request_pro_matches(_max_id: int) -> dict:
 
 def get_match_info(_match_id: int) -> dict:
 	url_match = "https://api.opendota.com/api/matches/{0}".format(_match_id)
-	match = urllib.request.urlopen(url_match).read().decode('utf8')
+	req = urllib.request.Request(url_match)
+	req.add_header('User-Agent', 'super happy dosBot')
+	match = urllib.request.urlopen(req).read().decode('utf8')
 	return json.loads(match)
+
+
+def get_player_recent_history(_player_id: int) -> dict:
+	url = "https://api.opendota.com/api/players/{0}/recentMatches".format(_player_id)
+	req = urllib.request.Request(url)
+	req.add_header('User-Agent', 'super happy dosBot')
+	history = urllib.request.urlopen(req).read().decode('utf8')
+	return json.loads(history)
