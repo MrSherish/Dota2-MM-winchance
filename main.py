8e37fc18d7173ae4d8e25be991f7969ad269c191
Mitrius <S.Strehlau@europe.com>
2017-06-05 18:26:09 +0200

    Dodany prosty klasyfikator(SVM)

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-import random
+from scipy import random
+from sklearn import svm
 
 import requests
 
+
+def classify(_train_data: list, _test_data: list) -> list:
+	svc = svm.SVC(kernel='poly', degree=3)
+	dota_x = list()
+	dota_y = list()
+	for _entry in _train_data:
+		dota_x.append(_entry[1:-1])
+		dota_y.append(_entry[-1])
+	svc.fit(X=dota_x, y=dota_y)
+	print("uczenie zakończone")
+	dota_x = list()
+	dota_y = list()
+	for _entry in _test_data:
+		dota_x.append(_entry[1:-1])
+		dota_y.append(_entry[-1])
+	predicted = svc.predict(dota_x).tolist()
+	print("predicted: " + str(predicted))
+	print("ref:" + str(dota_y))
+	return predicted
+
+
 # Pobieramy info o pro grach, o konkretnych meczach i tworzymy dwójkę gracza i jego osiągnięć w meczu
 if __name__ == "__main__":
-	pro_games = random.sample(requests.request_pro_matches(90000000), 5)
+	pro_games = random.choice(requests.request_pro_matches(90000000), 5)
 	pro_matches = map(lambda x: requests.get_match_info(x['match_id']), pro_games)
 	pro_players = [item['players'] for item in pro_matches]
 	pro_players = [item for sublist in pro_players for item in sublist]
@@ -26,4 +48,7 @@ if __name__ == "__main__":
 		row.append(entry[2])
 		data.append(row)
 
-	print("pobrano {0} wierszy".format((len(data))))
+	random.shuffle(data)
+	train_data = data[:-10]
+	test_data = data[-10:]
+	classify(train_data, test_data)
